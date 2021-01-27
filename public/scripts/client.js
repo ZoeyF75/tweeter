d69c040e446ecb4b7887715a831fc63c4cf10f28
Zoey Farago <zf.zoodle@gmail.com>
2021-01-27 23:14:41 +0000

    Ajax post progress

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,30 +5,6 @@
  */
 
 // Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
 
 const renderTweets = function(tweets) {
 // loops through tweets, calls createTweetElement for each tweet, takes return value and appends it to the tweets container
@@ -39,7 +15,6 @@ for (item of tweets) { // console.log => [object, object];
 }
 
 const createTweetElement = function(tweet) {
-  // console.log(moment(tweet['created_at']).fromNow());
   let $tweet = $(
     `<article class="tweet">
       <header> 
@@ -60,25 +35,28 @@ const createTweetElement = function(tweet) {
           </footer>
     </article>`
   );
- 
-  // let $tweet = $(`<div class="tweet"></div>`);
-  // let $name = $(`<header class="name">${tweet['user']['name']}</header>`);
-  // let $avatar = $(`<img src="">{$tweet.avatar}</img>`);
-  // let $handle = $(`<span class="handle">${tweet['user']['handle']}</span>`);
-  // $name.append($handle); //put the handle inside the name tag 
-  // $tweet.append($name); //put the whole name DOM object inside the tweet DOM object 
-
-  //  let $tweet = {
-  //   name: tweet['user']['name'],
-  //   avatar :tweet['user']['avatars'],
-  //   handle: tweet['user']['handle'],
-  //   content: tweet['content']['text'],
-  //   time: tweet['created_at']
-  //}
-
 return $tweet;
 }
 
 $(document).ready(function() { //waits for DOM
   renderTweets(data);
+  $(function() {
+    const $button = $('#post'); //submit button with post id
+    $button.on('click', function () { //when its clicked
+      console.log('Submit activated, performing ajax call...');
+      event.preventDefault(); //prevents page reload
+      const formData = {
+        'tweet-text': $('textarea[name=text]').serialize()
+      } 
+      console.log(formData);
+      $.ajax('/index.html', 'POST')
+      .then(function (formData) {
+        console.log('Success: ', formData );
+        // $(".tweetcontainer.".append(get(renderTweets(formData))));
+      });
+    });
+  });
 });
+
+
+
